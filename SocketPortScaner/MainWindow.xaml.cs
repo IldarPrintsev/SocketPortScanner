4fc7f550e324850d20bcb99e8c493ac9710dbe2b
NicoHayes <nicohayes9@gmail.com>
2019-01-11 12:48:01 +0300

    Commit 1.0

---
+++ SocketPortScaner/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+
+namespace SocketPortScaner
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<PortInfo> pi = MainWindow.GetOpenPort();
+            listview_scaner.ItemsSource = pi;
+        }
+
+        private static List<PortInfo> GetOpenPort()
+        {
+            IPGlobalProperties properties = IPGlobalProperties.GetIPGlobalProperties();
+            IPEndPoint[] tcpEndPoints = properties.GetActiveTcpListeners();
+            TcpConnectionInformation[] tcpConnections = properties.GetActiveTcpConnections();
+
+            return tcpConnections.Select(p =>
+            {
+                return new PortInfo(
+                    i: p.LocalEndPoint.Port,
+                    local: String.Format("{0}:{1}", p.LocalEndPoint.Address, p.LocalEndPoint.Port),
+                    remote: String.Format("{0}:{1}", p.RemoteEndPoint.Address, p.RemoteEndPoint.Port),
+                    state: p.State.ToString());
+            }).ToList();
+        }
+    }
+
+    class PortInfo
+    {
+        public int PortNumber { get; set; }
+        public string Local { get; set; }
+        public string Remote { get; set; }
+        public string State { get; set; }
+
+        public PortInfo(int i, string local, string remote, string state)
+        {
+            PortNumber = i;
+            Local = local;
+            Remote = remote;
+            State = state;
+        }
+    }
+}
